4eaf1fbbfad8dd5d162dfc50a553fa12f985452e
Lucile Garrigoux <lgarrigoux@hotmail.com>
2021-12-30 18:17:36 +0100

    Implemented postgres database and user model.

--- accounts/models.py
+++ accounts/models.py
@@ -14,34 +14,42 @@ class CustomUserManager(UserManager):
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
-    username = CIEmailField(
+    SALES = "sa"
+    SUPPORT = "su"
+
+    USER_TYPE = (
+        (SALES, 'Sales'), (SUPPORT, 'Support')
+        )
+
+    email = CIEmailField(
         "email address",
         unique=True,
-        max_length=150,
+        max_length=100,
         help_text="Required. 100 characters or fewer.",
         error_messages={
             "unique": "A user with that username already exists.",
         },
     )
 
-    first_name = models.CharField("first name", max_length=150, blank=True)
-    last_name = models.CharField("last name", max_length=150, blank=True)
+    first_name = models.CharField("first name", max_length=25)
+    last_name = models.CharField("last name", max_length=25)
+
+    # TODO: func - check entered phone number
+    phone_number = models.CharField("phone nomber", max_length=20)
 
     is_staff = models.BooleanField(
         "staff status",
         default=False,
         help_text="Designates whether the user can log into this admin site.",
     )
-    is_active = models.BooleanField(
-        "active",
-        default=True,
-        help_text=
-            "Designates whether this user should be treated as active. Unselect this instead of deleting accounts.",
-    )
-    # date_joined = models.DateTimeField("date joined", default=timezone.now)
+    role = models.CharField(choices=USER_TYPE, max_length=10)
 
-    USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["first_name", "last_name"]
+    date_created = models.DateTimeField("date joined", default=timezone.now)
+    # TODO: func - update updated
+    date_updated = models.DateTimeField("date joined", default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "phone_number"]
 
     class Meta:
         verbose_name = "user"
